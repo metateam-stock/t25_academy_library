140df630b4e69b9b85dd6b787589d8dc9dfc2f04
Morinobu_Henna <morinobu.henna@metateam.co.jp>
2026-05-11 17:32:14 +0900

    登録処理を追加

--- src/main/java/jp/co/metateam/library/controller/BookController.java
+++ src/main/java/jp/co/metateam/library/controller/BookController.java
@@ -1,7 +1,7 @@
 package jp.co.metateam.library.controller;
-
+ 
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,44 +11,50 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
+ 
 import jakarta.validation.Valid;
 import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.service.BookMstService;
 import lombok.extern.log4j.Log4j2;
-
+ 
 /**
  * 書籍関連クラス
  */
 @Log4j2
 @Controller
 public class BookController {
-    
+   
     private final BookMstService bookMstService;
-
-    @Autowired
+ 
+    @Autowired//springが自動で入力
     public BookController(BookMstService bookMstService){
         this.bookMstService = bookMstService;
     }
-
-    @GetMapping("/book/index")
+ 
+    @GetMapping("/book/index")//書籍画面を表示
     public String index(Model model) {
         // 書籍を全件取得
         List<BookMstDto> bookMstList = this.bookMstService.findAvailableWithStockCount();
-        
-        model.addAttribute("bookMstList", bookMstList);
-
+       
+        model.addAttribute("bookMstList", bookMstList);//HTMLへの橋渡し
         return "book/index";
     }
-
-    @GetMapping("/book/add")
+ 
+    @GetMapping("/book/add")//登録画面を表示
     public String add(Model model) {
-        if (!model.containsAttribute("bookMstDto")) {
-            model.addAttribute("bookMstDto", new BookMstDto());
+        if (!model.containsAttribute("bookMstDto")) {//モデルにbookMstDtoがない場合
+            model.addAttribute("bookMstDto", new BookMstDto()); //空の箱をつくってHTMLに渡す
         }
-
+ 
         return "book/add";
     }
     
+    //登録処理
+    @PostMapping("/book/add")
+    public String add(BookMstDto bookMstDto){
+        this.bookMstService.save(bookMstDto);//DBに登録お願いする
+        return "redirect:/book/index";//一覧に戻る（書籍画面）
+    }
 }
+ 
