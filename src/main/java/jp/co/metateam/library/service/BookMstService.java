396b9fdbfe420496de867e8303820d3e7c7a0c7f
MT 牧野 彩華 <ayana.makino@metateam.co.jp>
2026-05-14 21:39:50 +0900

    書籍登録機能の追加修正

--- src/main/java/jp/co/metateam/library/service/BookMstService.java
+++ src/main/java/jp/co/metateam/library/service/BookMstService.java
@@ -2,11 +2,15 @@ package jp.co.metateam.library.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import io.micrometer.common.util.StringUtils;
 import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.BookMstDto;
 import jp.co.metateam.library.repository.BookMstRepository;
@@ -15,12 +19,12 @@ import jp.co.metateam.library.repository.BookMstRepository;
 public class BookMstService {
 
     private final BookMstRepository bookMstRepository;
-
+    
     @Autowired
-    public BookMstService(BookMstRepository bookMstRepository) {
+    public BookMstService(BookMstRepository bookMstRepository){
         this.bookMstRepository = bookMstRepository;
     }
-
+    
     public List<BookMstDto> findAvailableWithStockCount() {
         List<BookMst> books = this.bookMstRepository.findLimitedBook();
         List<BookMstDto> bookMstDtoList = new ArrayList<BookMstDto>();
@@ -38,16 +42,18 @@ public class BookMstService {
 
         return bookMstDtoList;
     }
-
-    // データベースへの保存
+        // データベースへの保存
     @Transactional
     public void save(BookMstDto dto) {
         BookMst entity = new BookMst();
-
+ 
         entity.setTitle(dto.getTitle());
         entity.setIsbn(dto.getIsbn());
-
+ 
         bookMstRepository.save(entity);
     }
-
+    
 }
+
+
+
