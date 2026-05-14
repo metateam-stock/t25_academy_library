ba3686ce27e0d384782642e305407f1c5010af85
MT 牧野 彩華 <ayana.makino@metateam.co.jp>
2026-05-14 21:43:27 +0900

    Fix: update bookmst dto

--- src/main/java/jp/co/metateam/library/model/BookMstDto.java
+++ src/main/java/jp/co/metateam/library/model/BookMstDto.java
@@ -2,8 +2,7 @@ package jp.co.metateam.library.model;
 
 import java.security.Timestamp;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,19 +13,14 @@ import lombok.Setter;
 @Getter
 @Setter
 public class BookMstDto {
- 
-    private Long id;
- 
-    @NotBlank(message = "ISBNは必須です")
-    @Size(min = 13, max = 13, message = "ISBNは13文字で入力してください")
-    @Pattern(regexp = "^[0-9]+$", message = "ISBNは半角数字のみで入力してください")
+    
+    private Long id; 
+    
     private String isbn;
- 
-    @NotBlank(message = "書籍名は必須です")
-    @Size(max = 255, message = "書籍名は255文字以下で入力してください")
+
     private String title;
- 
+    
     private Timestamp deletedAt;
- 
+
     private BookMst bookMst;
 }
